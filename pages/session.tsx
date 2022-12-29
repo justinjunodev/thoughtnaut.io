34f209b9fb54e771c8e2574e622aa5cd0a5dfdb2
Justin Juno <50022106+justinjunodev@users.noreply.github.com>
2022-12-28 21:12:51 -0600

    connect xata db and load prompts

--- pages/session.tsx
+++ pages/session.tsx
@@ -1,8 +1,30 @@
-export default function Session() {
+import { GetServerSideProps } from 'next'
+import { XataClient, PromptsRecord } from '../utils/xata'
+
+interface SessionProps {
+  prompts: PromptsRecord[]
+}
+
+export default function Session({ prompts }: SessionProps) {
+  console.log(prompts)
+
   return (
     <>
       <h1>Thoughtnaut</h1>
       <p>Session.</p>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const xata = new XataClient({ apiKey: process.env.XATA_API_KEY })
+  const prompts = await xata.db.prompts.getAll()
+  const daily = prompts?.filter((prompt) => prompt.is_daily)
+  const special = prompts?.filter((prompt) => !prompt.is_daily)
+
+  const a = Math.floor(Math.random() * special?.length)
+  let b = Math.floor(Math.random() * special?.length)
+  b == a ? (b = Math.floor(Math.random() * special?.length)) : b
+
+  return { props: { prompts: [...daily, special[a], special[b]] } }
 }
