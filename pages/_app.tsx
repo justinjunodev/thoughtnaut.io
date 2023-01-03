a2519e7343e35220b0861db78b7865c318ea886c
Justin Juno <50022106+justinjunodev@users.noreply.github.com>
2023-01-02 20:52:51 -0600

    add fathom analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import '@/styles/reset.css'
 import '@/styles/globals.css'
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
+import Router from 'next/router'
+import * as Fathom from 'fathom-client'
+
+Router.events.on('routeChangeComplete', (as, routeProps) => {
+  if (!routeProps.shallow) {
+    Fathom.trackPageview()
+  }
+})
 
 const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    Fathom.load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID as string, {
+      includedDomains: ['thoughtnaut.io'],
+    })
+  }, [])
+
   return <Component {...pageProps} />
 }
 
