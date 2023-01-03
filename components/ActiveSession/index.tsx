a79910663dafa698d5ac4e4aa86e3f2e2806b09f
Justin Juno <50022106+justinjunodev@users.noreply.github.com>
2023-01-03 08:36:40 -0600

    refactor active session and add initial styles

--- components/ActiveSession/index.tsx
+++ components/ActiveSession/index.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router'
 import Balancer from 'react-wrap-balancer'
 import { trackGoal } from 'fathom-client'
 import { PromptsRecord } from '@/utils/xata'
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
 
 interface ActiveSessionProps {
   prompts: PromptsRecord[]
@@ -14,45 +15,51 @@ const ActiveSession = ({ prompts }: ActiveSessionProps) => {
   const [currentPrompt, setCurrentPrompt] = useState(0)
   const router = useRouter()
 
-  function incrementPrompt() {
-    if (currentPrompt === 4) {
-      if (process.env.NODE_ENV === 'production') {
-        trackGoal(process.env.NEXT_PUBLIC_FATHOM_SESSION_ENDED as string, 0)
-      }
-
-      router.push('/success')
-    } else {
-      setCurrentPrompt(currentPrompt + 1)
+  function handleClick() {
+    if (process.env.NODE_ENV === 'production') {
+      trackGoal(process.env.NEXT_PUBLIC_FATHOM_SESSION_ENDED as string, 0)
     }
-  }
 
-  function decrementPrompt() {
-    setCurrentPrompt(currentPrompt - 1)
+    router.push('/success')
   }
 
   return (
     <>
-      <h2>
-        <Balancer>Prompt {currentPrompt + 1} of 5</Balancer>
-      </h2>
-      <p>
-        <Balancer ratio={0.2}>{prompts[currentPrompt].prompt}</Balancer>
+      <h2 className={styles.heading}>Prompt {currentPrompt + 1} of 5</h2>
+      <p className={styles.copy}>
+        <Balancer ratio={0.75}>{prompts[currentPrompt].prompt}</Balancer>
       </p>
-      <button
-        type="button"
-        onClick={() => decrementPrompt()}
-        disabled={currentPrompt === 0 ? true : false}
-      >
-        Previous
-      </button>
-      <button type="button" onClick={() => incrementPrompt()}>
-        Next
-      </button>
-      <p>
-        <Link href="/" className={styles.subcopyLink}>
-          Leave Session
-        </Link>
-      </p>
+      {currentPrompt === 4 ? (
+        <button
+          type="button"
+          onClick={() => handleClick()}
+          className={styles.button}
+        >
+          Complete Session
+        </button>
+      ) : (
+        <>
+          <button
+            type="button"
+            onClick={() => setCurrentPrompt(currentPrompt - 1)}
+            className={styles.buttonRound}
+          >
+            <GrFormPrevious />
+          </button>
+          <button
+            type="button"
+            onClick={() => setCurrentPrompt(currentPrompt + 1)}
+            className={styles.buttonRound}
+          >
+            <GrFormNext />
+          </button>
+          <p className={styles.subcopy}>
+            <Link href="/" className={styles.subcopyLink}>
+              Leave Session
+            </Link>
+          </p>
+        </>
+      )}
     </>
   )
 }
